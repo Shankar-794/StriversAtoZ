764c904736f0f02a3fbbb19c72610c44a5b05821
Shankar-794 <shankar9666188@gmail.com>
2026-04-08 14:31:16 +0530

    solved largest element in array

---
+++ largest-element-in-array/BruteForce.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+public class BruteForce{
+    public static void main(String[] args){
+		int[] arr = {1,4,2,3,5};
+		Arrays.sort(arr);
+		System.out.println(arr[arr.length-1]);
+	}
+		
+}
---
+++ largest-element-in-array/Optimal.java
@@ -0,0 +1,12 @@
+class Optimal{
+	public static void main(String[] args){
+		int[] arr = {1,4,2,3,5};
+		int max = arr[0];
+		for(int i=1;i<arr.length;i++){
+			if(arr[i]>max){
+				max = arr[i];
+			}
+		}
+		System.out.println(max);
+	}
+}
